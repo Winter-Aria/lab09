97d4e2e039d99804e2efc7ec043a705d0f0526f4
Aria <notariaepic@gmail.com>
2025-05-05 16:25:03 +1000

    did blab 9

--- settingss.php
+++
@@ -1,5 +0,0 @@
-<?php    
-$host = "localhost";         // because XAMPP runs the server locally
-$user = "root";          // default username for XAMPP's MySQL
-$pwd = "";              // default password is empty in XAMPP
-$sql_db = "your_db_name";  // replace with the actual name of your database
